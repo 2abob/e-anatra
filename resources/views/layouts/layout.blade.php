12ac1c2f60aa3693910008f13f8aeef940077a48
omirs <andriamahazonyaro@gmail.com>
2023-05-27 15:26:37 +0300

    commit

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -33,9 +33,9 @@
     <!-- partial:partials/_navbar.html -->
     <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
         <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-            <a class="navbar-brand brand-logo mr-5" href="/dashboard"><img src="{{ asset('images/soratra1.png') }}"
+            <a class="navbar-brand brand-logo mr-5" href="/dashboard"><img src="{{ asset('images/eanatra2.png') }}"
                                                                            class="mr-2" alt="logo"/></a>
-            <a class="navbar-brand brand-logo-mini" href="/dashboard"><img src="{{ asset('images/final.png') }}"
+            <a class="navbar-brand brand-logo-mini" href="/dashboard"><img src="{{ asset('images/eanatra.png') }}"
                                                                            alt="logo"/></a>
         </div>
         <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
